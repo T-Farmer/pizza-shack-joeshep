270a86f6ca9acb7def6c866bcb1f57707c9fee3f
T-Farmer <terrillfarmer@gmail.com>
2017-03-29 14:40:31 -0500

    pizza app boilerplate and initial setup

---
+++ app.js
@@ -0,0 +1,21 @@
+'use strict';
+
+require('dotenv').config();
+const express = require('express');
+const app = express();
+
+// pug configuration
+app.set('view engine', 'pug');
+
+// middleware
+app.use(express.static('public'));
+
+app.get('/', (req, res, next) => {
+  res.render('index');
+})
+
+
+const port = process.env.PORT || 3000;
+app.listen(port, () => {
+  console.log("Listening on port ${port}");
+});
