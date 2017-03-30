bb54d3f9073c6188dd9a29b5c9d69de9291dd5f0
T-Farmer <terrillfarmer@gmail.com>
2017-03-30 15:39:55 -0500

    added contact us route and ctrl

--- routes/contacts.js
+++ routes/contacts.js
@@ -1,1 +1,11 @@
 'use strict';
+
+const {Router} = require('express');
+
+const {show} = require('../controllers/contactsCtrl');
+
+const router = Router();
+
+router.get('/contacts', show);
+
+module.exports = router;
