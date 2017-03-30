4e9e7d76073474ec6405fcc24e74ac9062fe790c
T-Farmer <terrillfarmer@gmail.com>
2017-03-30 10:41:21 -0500

    puggin along with the pizza shack

--- app.js
+++ app.js
@@ -7,13 +7,34 @@ const app = express();
 // pug configuration
 app.set('view engine', 'pug');
 
+app.locals.company = 'Pizza Shack 🍕';
+app.locals.body = {};
+app.locals.body.magic = 'Fooooooo!';
+
 // middleware
 app.use(express.static('public'));
 
 app.get('/', (req, res, next) => {
   res.render('index');
-})
+});
 
+app.get('/about', (req, res, next) => {
+  res.render('about');
+});
+
+app.get('/contacts', (req, res, next) => {
+  res.render('contacts');
+});
+
+app.get('/login', (req, res, next) => {
+  res.render('login');
+});
+
+app.use((req, res) => {
+  res.render('404');
+});
+
+// end of middleware
 
 const port = process.env.PORT || 3000;
 app.listen(port, () => {
